039405262a7cf9ddfe656f248092cb1de2e93243
lidz <lindikhaya.maqolwana@gmail.com>
2021-02-10 17:08:58 +0200

    Uploading a website that was  archived

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+console.log('we are live');
+
+console.log('its working')
+
+let travel = document.getElementById('travel_fashion');
+travel.addEventListener('click', function(){
+    location = travel.getAttribute('href');
+    location = 'pages/travel_fashion.html';
+})
